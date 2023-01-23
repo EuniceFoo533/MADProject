94b719a2e533d9e0f5a0038c86ccec589f2da708
EuniceFoo533 <b032110050@student.utem.edu.my>
2023-01-24 00:09:36 +0800

    Add Navigation Menu and drawer layout

--- app/src/main/java/com/example/fragment/TimerFragment.java
+++ app/src/main/java/com/example/fragment/TimerFragment.java
@@ -2,15 +2,36 @@ package com.example.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 
 public class TimerFragment extends Fragment {
 
+    private static final long START_TIME_IN_MILIS = 1500000;
+
+    private ProgressBar progressBar;
+    private Button buttonStart, buttonReset,buttonSet;
+    private TextView textViewTimer;
+    private EditText editTimer;
+
+    private CountDownTimer countDownTimer;
+    private  boolean timerRunning;
+    private boolean timerStopping;
+
+    private long timeLeftMilis;
 
     public TimerFragment() {
         // Required empty public constructor
@@ -22,10 +43,122 @@ public class TimerFragment extends Fragment {
 
     }
 
+
+    // Use to return root view
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        progressBar = getView().findViewById(R.id.progressBar2);
+        textViewTimer = getView().findViewById(R.id.tvCountDown);
+
+        buttonStart = getView().findViewById(R.id.buttonStart);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (timerRunning) {
+                    pauseTimer();
+                } else {
+                    startTimer();
+                }
+            }
+        });
+        buttonReset = getView().findViewById(R.id.buttonStop);
+        buttonReset.setVisibility(View.INVISIBLE);
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetTimer();
+                progressBar.setProgress(1500000);
+            }
+        });
+        updateCountDownText();
+
+        editTimer = getView().findViewById(R.id.editTimer);
+        buttonSet = getView().findViewById(R.id.buttonSet);
+        buttonSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                long time = Long.parseLong(editTimer.getText().toString())*60000;
+                timeLeftMilis = time;
+                editTimer.setText("");
+            }
+        });
+
+    }
+
+    //Method start timer
+    private void startTimer()
+    {
+        countDownTimer = new CountDownTimer(timeLeftMilis,1000) {
+            @Override
+            public void onTick(long msUntilFinished) {
+                timeLeftMilis = msUntilFinished;
+                updateCountDownText();
+                progressBar.setMax((int)START_TIME_IN_MILIS);
+                progressBar.setProgress((int)msUntilFinished);
+
+            }
+
+            @Override
+            public void onFinish() {
+                timerRunning = false;
+                buttonStart.setText("Start");
+                buttonStart.setVisibility(View.INVISIBLE);
+                buttonReset.setVisibility(View.VISIBLE);
+                progressBar.setProgress(0);
+
+            }
+        }.start();
+
+        timerRunning = true;
+        buttonStart.setText("Pause");
+        buttonStart.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.pause_timer, 0);
+        buttonReset.setVisibility(View.INVISIBLE);
+
+    }
+
+    //Method pause the timer
+    private void pauseTimer()
+    {
+        countDownTimer.cancel();
+        timerRunning = false;
+        buttonStart.setText("Resume");
+        buttonStart.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.start_timer, 0);
+        buttonReset.setVisibility(View.VISIBLE);
+
+    }
+
+    private void resetTimer()
+    {
+        timeLeftMilis = START_TIME_IN_MILIS;
+        updateCountDownText();
+        buttonReset.setVisibility(View.INVISIBLE);
+        buttonStart.setVisibility(View.VISIBLE);
+        buttonStart.setText("Start");
+        buttonStart.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.start_timer, 0);
+
+
+
+    }
+
+    private void updateCountDownText()
+    {
+
+        int minutes = (int) (timeLeftMilis/1000)/60;
+        int seconds = (int) (timeLeftMilis/1000)%60;
+        int hours = 0;
+
+        String timeLeftFormatted = String.format(Locale.getDefault(),"%02d:%02d:%02d",hours,minutes,seconds);
+        textViewTimer.setText(timeLeftFormatted);
+
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_timer, container, false);
     }
 }
