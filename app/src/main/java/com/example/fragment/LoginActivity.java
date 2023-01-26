1905da2fdecae8e626c1338d8c1a97917cab3b77
EuniceFoo533 <b032110050@student.utem.edu.my>
2023-01-26 11:09:04 +0800

    Add Navigation Menu and drawer layout

--- app/src/main/java/com/example/fragment/LoginActivity.java
+++ app/src/main/java/com/example/fragment/LoginActivity.java
@@ -1,14 +1,158 @@
 package com.example.fragment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText edtEmail, edtPassword;
+    Button btnLogin;
+    TextView tvForgetPassword,tvRegister;
+    ProgressBar progressBar;
+
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    DatabaseReference reference;
+
+    String email, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edtEmail = findViewById(R.id.editEmailAddress);
+        edtPassword = findViewById(R.id.editPassword);
+
+        //Button for login
+        btnLogin = findViewById(R.id.buttonLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginAccountInFirebase();
+            }
+        });
+
+        //Reset password
+        tvForgetPassword = findViewById(R.id.textViewForgetPassword);
+        tvForgetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        //Button register
+        tvRegister = findViewById(R.id.textViewRegister);
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
+
+        progressBar = findViewById(R.id.progressBar);
+
     }
+
+    private void loginAccountInFirebase()
+    {
+        email = edtEmail.getText().toString();
+        password = edtPassword.getText().toString();
+
+        if(TextUtils.isEmpty(email))
+        {
+            edtEmail.setError("Email is required");
+        }
+
+        if(TextUtils.isEmpty(password))
+        {
+            edtPassword.setError("Password is required");
+        }
+
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            edtEmail.setError("Email is invalid");
+
+        }
+
+        if(password.length()<=7)
+        {
+            edtPassword.setError("Password must contain 8 characters");
+
+        }
+
+        else{
+            email = edtEmail.getText().toString();
+            password = edtPassword.getText().toString();
+
+            changesInProgress(true);
+
+            firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    changesInProgress(false);
+
+                    if(task.isSuccessful())
+                    {
+                        if(firebaseAuth.getCurrentUser().isEmailVerified())
+                        {
+                            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                        }
+
+                        else if(!firebaseAuth.getCurrentUser().isEmailVerified())
+                        {
+                            Toast.makeText(LoginActivity.this,"Email not verified.Please check your inbox.",Toast.LENGTH_SHORT).show();
+                        }
+
+                        else {
+                            Toast.makeText(LoginActivity.this,"User not found.",Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+
+                    else
+                    {
+                        Toast.makeText(LoginActivity.this,"Invalid username and password",Toast.LENGTH_SHORT).show();
+                    }
+
+
+                }
+            });
+        }
+
+    }
+
+    private void changesInProgress(boolean inProgress)
+    {
+        if(inProgress)
+        {
+            progressBar.setVisibility(View.VISIBLE);
+            btnLogin.setVisibility(View.GONE);
+        }
+        else
+        {
+            progressBar.setVisibility(View.GONE);
+            btnLogin.setVisibility(View.VISIBLE);
+        }
+    }
+
 }
--- app/src/main/java/com/example/fragment/RegisterActivity.java
+++ app/src/main/java/com/example/fragment/RegisterActivity.java
@@ -1,14 +1,170 @@
 package com.example.fragment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.airbnb.lottie.L;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText editEmail,editPassword, editConfirmPassword;
+    TextView tvLogin;
+    Button btnRegister;
+    ProgressBar progressBar;
+
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+    String email, password, confirmPassword;
+    final Pattern PASSWORD_PATTERN =
+            Pattern.compile("^" +
+                    "(?=.*[@#$%^&+=])" +     // at least 1 special character
+                    "(?=\\S+$)" +            // no white spaces
+                    ".{4,}" +                // at least 4 characters
+                    "$");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        editEmail = findViewById(R.id.editEmail);
+        editPassword = findViewById(R.id.editPassword);
+        editConfirmPassword = findViewById(R.id.editConfirmPassword);
+
+        // Go to login page
+        tvLogin = findViewById(R.id.textViewLogin);
+        tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+
+        // Register an account
+        btnRegister = findViewById(R.id.buttonRegister);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createAccountInFirebase();
+            }
+        });
+
+        progressBar = findViewById(R.id.progressBar);
+
+    }
+
+    private void createAccountInFirebase() {
+
+        email = editEmail.getText().toString();
+        password = editPassword.getText().toString();
+        confirmPassword = editConfirmPassword.getText().toString();
+
+        if(email.matches("")  && password.matches("") && confirmPassword.matches(""))
+        {
+            Toast.makeText(RegisterActivity.this,"Every fields required to fill",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        else if(email.matches(""))
+        {
+            Toast.makeText(RegisterActivity.this,"Email required to fill",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        else if(password.matches(""))
+        {
+            Toast.makeText(RegisterActivity.this,"Password required to fill",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        else if(confirmPassword.matches(""))
+        {
+            Toast.makeText(RegisterActivity.this,"Confirm password required to fill",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        else if(!password.equals(confirmPassword))
+        {
+            Toast.makeText(RegisterActivity.this,"Password and confirm password not matching.",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        else if (password.length() < 8) {
+            Toast.makeText(RegisterActivity.this,"Password is too weak. \nAt least 8 characters",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        else if (!PASSWORD_PATTERN.matcher(password).matches()) {
+            Toast.makeText(RegisterActivity.this,"Must contain at least 1 special character",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            Toast.makeText(RegisterActivity.this,"Invalid Email Address",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        else
+        {
+            firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    changesInProgress(false);
+                    if(task.isSuccessful())
+                    {
+                        changesInProgress(true);
+                        Toast.makeText(RegisterActivity.this,"Successfully register",Toast.LENGTH_SHORT).show();
+
+                        firebaseAuth.getCurrentUser().sendEmailVerification();
+                        firebaseAuth.signOut();
+
+                        Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                        startActivity(intent);
+
+
+
+                    }
+                    else
+                    {
+                        Toast.makeText(RegisterActivity.this,task.getException().getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+
+
+
+    }
+
+    private void changesInProgress(boolean inProgress)
+    {
+        if(inProgress)
+        {
+            progressBar.setVisibility(View.VISIBLE);
+            btnRegister.setVisibility(View.GONE);
+        }
+        else
+        {
+            progressBar.setVisibility(View.GONE);
+            btnRegister.setVisibility(View.VISIBLE);
+        }
     }
 }
